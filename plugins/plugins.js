1ce85c49bf8e6244aebf8bfef07a6130a5798ccd
nitrid <alikemalkaraca@gmail.com>
2021-09-06 18:39:05 +0300

    .

---
+++ plugins/plugins.js
@@ -0,0 +1,22 @@
+//*******************************************************/
+// Plugins - 05.07.2021 - Ali Kemal KARACA
+// Example
+// import core from 'gensrv'
+
+// export default class plugins 
+// {
+//     constructor()
+//     {        
+       
+//     }    
+// }
+//*******************************************************/
+import core from 'gensrv'
+
+export default class plugins 
+{
+    constructor()
+    {        
+        console.log(core)
+    }    
+}
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import server from 'gensrv'
 import config from './config.js'
+import fs from 'fs'
 
-let gensrv = new server(config);
+let gensrv = new server.default(config);
 gensrv.listen(80);
+
+//PLUGIN YAPISI ******************************/
+fs.readdirSync('./plugins').forEach(file => 
+{
+    import('./plugins/' + file).then(module =>
+    {
+        Object.keys(module).forEach(element => 
+        {
+            gensrv.plugins[element] = new module[element];
+        });
+    })
+});
+//*******************************************/
