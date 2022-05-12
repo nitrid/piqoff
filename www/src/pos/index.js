f0bf908055a1aa55933270197fc429d7c7b81d36
nitrid <alikemalkaraca@gmail.com>
2022-05-13 00:14:28 +0300

    .

--- www/src/pos/index.js
+++ www/src/pos/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import App from './lib/app.js'
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(<App />)
+ReactDOM.render(<App />,document.getElementById("root"));
