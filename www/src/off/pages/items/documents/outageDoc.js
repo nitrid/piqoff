29a0488800d5a846b442f1af486409827d25e637
recepkaraca1 <receeep7@gmail.com>
2022-06-06 21:10:20 +0300

    ...

--- www/src/off/pages/items/documents/outageDoc.js
+++ www/src/off/pages/items/documents/outageDoc.js
@@ -467,6 +467,8 @@ export default class outageDoc extends React.Component
                                                 {                                                    
                                                     tmpConfObj1.content = (<div style={{textAlign:"center",fontSize:"20px"}}>{this.t("msgSaveResult.msgSuccess")}</div>)
                                                     await dialog(tmpConfObj1);
+                                                    this.btnSave.setState({disabled:true});
+                                                    this.btnNew.setState({disabled:false});
                                                 }
                                                 else
                                                 {
