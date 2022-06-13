d668781ebf81c85e30bf4134020d61d8fb4158c9
recepkaraca1 <receeep7@gmail.com>
2022-06-13 17:29:37 +0300

    ...

--- www/src/pos/meta/lang/tr.js
+++ www/src/pos/meta/lang/tr.js
@@ -20,15 +20,18 @@ export const langTr =
     msgInvalidUser : "Kullanıcı Adı veya Şifre Hatalı.",
     txtCardRead :"Lütfen Kullanıcı Kartınızı Okutunuz.",
     price : "Fiyat",
+    quantity : "Miktar",
     msgCustomerNotFound : 
     {
         title : "Dikkat",
-        msg : "Müşteri Bulunamadı !"
+        msg : "Müşteri Bulunamadı !",
+        btn01 : "Tamam",
     },
     msgZeroValidation :
     {
         title : "Uyarı",
-        msg : "Miktar sıfır giremezsiniz !"
+        msg : "Miktar sıfır giremezsiniz !",
+        btn01 : "Tamam",
     },
     msgPriceNotFound : 
     {
@@ -37,6 +40,28 @@ export const langTr =
         btn01 : "Evet",
         btn02 : "Hayır"
     },
-
-
+    msgBarcodeNotFound : 
+    {
+        title : "Uyarı",
+        msg : "Okuttuğunuz Barkod Sistemde Bulunamadı !",
+        btn01 : "Tamam",
+    },
+    msgSpecialTicketAlert : 
+    {
+        title : "Uyarı",
+        msg : "Özel etiketli ürünlerde herhangi bir değişiklik yapamazsınız !",
+        btn01 : "Tamam",
+    },
+    msgMoneyChange : 
+    {
+        title : "Bilgi",
+        msg : "Para Üstü Veriniz.",
+        btn01 : "Tamam",
+    },
+    msgBigAmount :
+    {
+        title : "Bilgi",
+        msg : "Girmiş olduğunuz tutar, kalan tutardan daha büyük olamaz !",
+        btn01 : "Tamam",
+    }
 }
