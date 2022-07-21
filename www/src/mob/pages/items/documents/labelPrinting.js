dd65d209ae6787e6f2ea316808369d82bc2d46bf
recepkaraca1 <receeep7@gmail.com>
2022-07-21 18:44:31 +0300

    ..

---
+++ www/src/mob/pages/items/documents/labelPrinting.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import App from '../../../lib/app.js';
+import { labelCls,labelMainCls } from '../../../../core/cls/label.js';
+import moment from 'moment';
+
+import ScrollView from 'devextreme-react/scroll-view';
+import Toolbar from 'devextreme-react/toolbar';
+import Form, { Label,Item,EmptyItem } from 'devextreme-react/form';
+import TabPanel from 'devextreme-react/tab-panel';
+import { Button } from 'devextreme-react/button';
+
+import NdTextBox, { Validator, NumericRule, RequiredRule, CompareRule, EmailRule, PatternRule, StringLengthRule, RangeRule, AsyncRule } from '../../../../core/react/devex/textbox.js'
+import NdNumberBox from '../../../../core/react/devex/numberbox.js';
+import NdSelectBox from '../../../../core/react/devex/selectbox.js';
+import NdCheckBox from '../../../../core/react/devex/checkbox.js';
+import NdPopGrid from '../../../../core/react/devex/popgrid.js';
+import NdPopUp from '../../../../core/react/devex/popup.js';
+import NdGrid,{Column,Editing,Paging,Scrolling,KeyboardNavigation,Export} from '../../../../core/react/devex/grid.js';
+import NdButton from '../../../../core/react/devex/button.js';
+import NdDatePicker from '../../../../core/react/devex/datepicker.js';
+import NdImageUpload from '../../../../core/react/devex/imageupload.js';
+import { dialog } from '../../../../core/react/devex/dialog.js';
+import { datatable } from '../../../../core/core.js';
+import tr from '../../../meta/lang/devexpress/tr.js';
+
+export default class labelPrinting extends React.Component
+{
+    constructor()
+    {
+        super()
+        this.core = App.instance.core;
+        this.prmObj = this.param.filter({TYPE:1,USERS:this.user.CODE});
+        this.acsobj = this.access.filter({TYPE:1,USERS:this.user.CODE});
+        this.lblObj = new labelCls();
+        this.mainLblObj = new labelMainCls()
+        this.pageCount = 0;
+
+        this._cellRoleRender = this._cellRoleRender.bind(this)
+
+        this.frmOutwas = undefined;
+    }
+    async componentDidMount()
+    {
+        await this.core.util.waitUntil(0)
+    }
+    render()
+    {
+        return(
+            <div>
+                     
+            </div>
+        )
+    }
+}
