1907308ebe43724b37b2119c9ab6b77ef57c9078
recepkaraca1 <receeep7@gmail.com>
2022-08-10 14:41:11 +0300

    ...

--- www/src/off/pages/pos/operation/endOfDay.js
+++ www/src/off/pages/pos/operation/endOfDay.js
@@ -127,7 +127,7 @@ export default class endOfDay extends React.PureComponent
       else
       {
         let tmpCash
-        tmpCash = (parseFloat(Number(this.txtCash.value) + Number(this.txtAdvance.value)) - parseFloat(this.paymentData.where({'PAY_TYPE':0})))
+        tmpCash = (parseFloat(this.txtCash.value + this.txtAdvance.value) - parseFloat(this.paymentData.where({'PAY_TYPE':0})))
         this.Cash = tmpCash.toFixed(2)
         this.setState({Cash:tmpCash})
       }
