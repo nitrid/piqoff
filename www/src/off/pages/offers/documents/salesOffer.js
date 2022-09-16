9b47459ec566c00e94551df150190404ba7bad92
piqpos <piqposfr@gmail.com>
2022-09-16 03:48:04 +0200

    ...

--- www/src/off/pages/offers/documents/salesOffer.js
+++ www/src/off/pages/offers/documents/salesOffer.js
@@ -1706,7 +1706,7 @@ export default class salesOrder extends React.PureComponent
                                     onValueChanged={(async()=>
                                         {
                                         }).bind(this)}
-                                    data={{source:{select:{query : "SELECT TAG,DESIGN_NAME FROM [dbo].[LABEL_DESIGN] WHERE PAGE = '61'"},sql:this.core.sql}}}
+                                    data={{source:{select:{query : "SELECT TAG,DESIGN_NAME FROM [dbo].[LABEL_DESIGN] WHERE PAGE = '07'"},sql:this.core.sql}}}
                                     param={this.param.filter({ELEMENT:'cmbDesignList',USERS:this.user.CODE})}
                                     access={this.access.filter({ELEMENT:'cmbDesignList',USERS:this.user.CODE})}
                                     >
@@ -1742,6 +1742,8 @@ export default class salesOrder extends React.PureComponent
                                                             "CONVERT(NVARCHAR,AMOUNT) AS AMOUNTF, " +
                                                             "ISNULL((SELECT TOP 1 NAME FROM COMPANY),'') AS FIRMA, " +
                                                             "REPLACE(ISNULL((SELECT ADDRESS1 + ' | ' + ADDRESS2  + ' | ' + TEL + ' | ' + MAIL FROM COMPANY),''),'|', CHAR(13)) AS BASLIK," +
+                                                            "REPLACE(ISNULL((SELECT ADRESS + ' | ' + ZIPCODE + ' ' + CITY +  '/' + COUNTRY FROM CUSTOMER_ADRESS_VW_01 WHERE CUSTOMER_ADRESS_VW_01.CUSTOMER = DOC_OFFERS_VW_01.INPUT AND TYPE = 0),''),'|', CHAR(13)) AS ADDRESS," +
+                                                            "ISNULL((SELECT TOP 1 PHONE1 FROM CUSTOMER_OFFICAL WHERE CUSTOMER_OFFICAL.CUSTOMER = DOC_OFFERS_VW_01.INPUT AND TYPE = 0),'') AS CONTACT, " +
                                                             "ISNULL((SELECT TOP 1 PATH FROM LABEL_DESIGN WHERE TAG = @DESIGN),'') AS PATH " +
                                                             "FROM DOC_OFFERS_VW_01 " +
                                                             "WHERE DOC_GUID=@DOC_GUID ORDER BY LINE_NO ASC",
@@ -1749,6 +1751,7 @@ export default class salesOrder extends React.PureComponent
                                                     value:  [this.docObj.dt()[0].GUID,this.cmbDesignList.value]
                                                 }
                                                 let tmpData = await this.core.sql.execute(tmpQuery) 
+                                                console.log(tmpData)
                                                 this.core.socket.emit('devprint',"{TYPE:'REVIEW',PATH:'" + tmpData.result.recordset[0].PATH.replaceAll('\\','/') + "',DATA:" + JSON.stringify(tmpData.result.recordset) + "}",(pResult) => 
                                                 {
                                                     if(pResult.split('|')[0] != 'ERR')
