467ba5f0a13a5627848526ec774c719dda3be208
nitrid <alikemalkaraca@gmail.com>
2022-09-30 05:29:22 +0300

    .

--- www/src/pos/meta/print/print.js
+++ www/src/pos/meta/print/print.js
@@ -142,7 +142,7 @@ export function print()
                         font: "b",
                         style: "b",
                         align: "lt",
-                        size : [0,1],
+                        size : [1,0],
                         data: "Remise ".space(53) + "-" + (parseFloat(tmpRemise).toFixed(2) + "EUR").space(10,"s")
                     })
                 });
