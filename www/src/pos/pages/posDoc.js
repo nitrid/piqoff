8ac2f8dc067bd65ef45d8b08a97cafb645df49c0
nitrid <alikemalkaraca@gmail.com>
2023-04-06 02:02:40 +0300

    .

--- www/src/pos/pages/posDoc.js
+++ www/src/pos/pages/posDoc.js
@@ -3177,8 +3177,8 @@ export default class posDoc extends React.PureComponent
                                         cellElement.innerText = this.posObj.posSale.dt().length - cellInfo.rowIndex
                                     }}
                                     alignment={"center"}/>                                    
-                                    <Column dataField="ITEM_SNAME" caption={this.lang.t("grdList.ITEM_NAME")} width={270}/>
-                                    <Column dataField="QUANTITY" caption={this.lang.t("grdList.QUANTITY")} width={50}/>
+                                    <Column dataField="ITEM_SNAME" caption={this.lang.t("grdList.ITEM_NAME")} width={260}/>
+                                    <Column dataField="QUANTITY" caption={this.lang.t("grdList.QUANTITY")} width={60} cellRender={(e)=>{return e.value + " / " + e.data.UNIT_SHORT}}/>
                                     <Column dataField="PRICE" caption={this.lang.t("grdList.PRICE")} width={70} format={"#,##0.00" + Number.money.sign}/>
                                     <Column dataField="AMOUNT" alignment={"right"} caption={this.lang.t("grdList.AMOUNT")} width={60} format={"#,##0.00" + Number.money.sign}/>                                                
                                 </NdGrid>
@@ -7668,7 +7668,6 @@ export default class posDoc extends React.PureComponent
                                 </div>   
                             </div>
                         </div>     
-                      
                     </NdDialog>
                 </div>
             </div>
