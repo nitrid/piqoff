ed2b0e01a889c23f7c658628e860d72cbc08fda5
nitrid <alikemalkaraca@gmail.com>
2023-04-06 01:37:46 +0300

    .

--- www/src/pos/pages/posDoc.js
+++ www/src/pos/pages/posDoc.js
@@ -1194,7 +1194,7 @@ export default class posDoc extends React.PureComponent
                         this.posDevice.lcdPrint
                         ({
                             blink : 0,
-                            text :  parseFloat(Number(tmpPosSale[tmpPosSale.length - 1].QUANTITY)).toFixed(3).space(6) + "KG X " +
+                            text :  parseFloat(Number(tmpPosSale[tmpPosSale.length - 1].QUANTITY)).toFixed(3).space(6) + "kg X " +
                                     (parseFloat(Number(tmpPosSale[tmpPosSale.length - 1].PRICE) - (Number(tmpPosSale[tmpPosSale.length - 1].DISCOUNT) / Number(tmpPosSale[tmpPosSale.length - 1].QUANTITY))).toFixed(2) + "EUR").space(8) + " " +
                                     tmpPosSale[tmpPosSale.length - 1].ITEM_NAME.toString().space(11) + "=" +  (parseFloat(Number(tmpPosSale[tmpPosSale.length - 1].TOTAL)).toFixed(2) + "EUR").space(8,"s")
                         })
