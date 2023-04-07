22d5a5242ef8a2790afef545e57fd509f9487ee0
nitrid <alikemalkaraca@gmail.com>
2023-04-08 00:44:31 +0300

    .

--- www/src/pos/pages/posDoc.js
+++ www/src/pos/pages/posDoc.js
@@ -490,7 +490,7 @@ export default class posDoc extends React.PureComponent
                         blink : 0,
                         text :  parseFloat(Number(pData.rowData.QUANTITY)).toFixed(3).space(6) + "kg X " +
                                 (parseFloat(Number(pData.rowData.PRICE) - (Number(pData.rowData.DISCOUNT) / Number(pData.rowData.QUANTITY))).toFixed(2) + "EUR").space(9,"s") +
-                                tmpPosSale[tmpPosSale.length - 1].ITEM_NAME.toString().space(11) + "=" +  (parseFloat(Number(pData.rowData.TOTAL)).toFixed(2) + "EUR").space(8,"s")
+                                pData.rowData.ITEM_NAME.toString().space(11) + "=" +  (parseFloat(Number(pData.rowData.TOTAL)).toFixed(2) + "EUR").space(8,"s")
                     })
                 }
                 else
