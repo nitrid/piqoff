3e917e37c4e4fa6b7511a74e367ed0f4964e6ebd
nitrid <alikemalkaraca@gmail.com>
2023-04-28 18:47:22 +0300

    .

--- www/src/tab/lib/page.js
+++ www/src/tab/lib/page.js
@@ -18,53 +18,53 @@ export default class Page extends React.Component
   openPage(pData)
   {
     this.data = pData
-    this.page = React.lazy(() => import("../pages/" + pData.path).then(async (obj)=>
-    {
-      //SAYFA YÜKLENMEDEN ÖNCE PARAMETRE, DİL, YETKİLENDİRME DEĞERLERİ GETİRİLİP CLASS PROTOTYPE A SET EDİLİYOR.
-      let tmpPrm = new param(prm);
-      await tmpPrm.load({APP:'TAB'})
+    // this.page = React.lazy(() => import("../pages/" + pData.path).then(async (obj)=>
+    // {
+    //   //SAYFA YÜKLENMEDEN ÖNCE PARAMETRE, DİL, YETKİLENDİRME DEĞERLERİ GETİRİLİP CLASS PROTOTYPE A SET EDİLİYOR.
+    //   let tmpPrm = new param(prm);
+    //   await tmpPrm.load({APP:'TAB'})
       
-      let tmpAcs = new access(acs);
-      await tmpAcs.load({APP:'TAB'})
+    //   let tmpAcs = new access(acs);
+    //   await tmpAcs.load({APP:'TAB'})
       
-      obj.default.prototype.param = tmpPrm.filter({PAGE:pData.id});
-      obj.default.prototype.sysParam = tmpPrm.filter({TYPE:0});
-      obj.default.prototype.access = tmpAcs.filter({PAGE:pData.id});
-      obj.default.prototype.user = this.core.auth.data;
-      obj.default.prototype.lang = App.instance.lang;
-      obj.default.prototype.t = App.instance.lang.getFixedT(null,null,pData.id)
-      obj.default.prototype.pagePrm = pData.pagePrm;
+    //   obj.default.prototype.param = tmpPrm.filter({PAGE:pData.id});
+    //   obj.default.prototype.sysParam = tmpPrm.filter({TYPE:0});
+    //   obj.default.prototype.access = tmpAcs.filter({PAGE:pData.id});
+    //   obj.default.prototype.user = this.core.auth.data;
+    //   obj.default.prototype.lang = App.instance.lang;
+    //   obj.default.prototype.t = App.instance.lang.getFixedT(null,null,pData.id)
+    //   obj.default.prototype.pagePrm = pData.pagePrm;
 
-      obj.default.prototype.init = (function()
-      {
-        let tmpCached = obj.default.prototype.init;
-        return function()
-        {          
-          tmpCached.apply(this,arguments)
-          this.emit('onInit')
-        }
-      }());
-      //EVENT PAGE - ALI KEMAL KARACA - 25.01.2022 *****/
-      obj.default.prototype.listeners = Object();
-      obj.default.prototype.on = function(pEvt, pCallback) 
-      {
-        if (!this.listeners.hasOwnProperty(pEvt))
-        this.listeners[pEvt] = Array();
-        this.listeners[pEvt].push(pCallback); 
-      }
-      obj.default.prototype.emit = function(pEvt, pParams)
-      {
-          if (pEvt in this.listeners) 
-          {
-              let callbacks = this.listeners[pEvt];
-              for (var x in callbacks)
-              {
-                  callbacks[x](pParams);
-              }
-          } 
-      }
-      return obj;
-    }))    
+    //   obj.default.prototype.init = (function()
+    //   {
+    //     let tmpCached = obj.default.prototype.init;
+    //     return function()
+    //     {          
+    //       tmpCached.apply(this,arguments)
+    //       this.emit('onInit')
+    //     }
+    //   }());
+    //   //EVENT PAGE - ALI KEMAL KARACA - 25.01.2022 *****/
+    //   obj.default.prototype.listeners = Object();
+    //   obj.default.prototype.on = function(pEvt, pCallback) 
+    //   {
+    //     if (!this.listeners.hasOwnProperty(pEvt))
+    //     this.listeners[pEvt] = Array();
+    //     this.listeners[pEvt].push(pCallback); 
+    //   }
+    //   obj.default.prototype.emit = function(pEvt, pParams)
+    //   {
+    //       if (pEvt in this.listeners) 
+    //       {
+    //           let callbacks = this.listeners[pEvt];
+    //           for (var x in callbacks)
+    //           {
+    //               callbacks[x](pParams);
+    //           }
+    //       } 
+    //   }
+    //   return obj;
+    // }))    
   }
   shouldComponentUpdate(nextProps) 
   {
