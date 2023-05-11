0d166ece04a5774a1f5cd0a02264d075461ff4b3
recepkaraca1 <receeep7@gmail.com>
2023-05-11 22:51:59 +0300

    ..

--- www/src/off/pages/invoices/documents/salesInvoice.js
+++ www/src/off/pages/invoices/documents/salesInvoice.js
@@ -4279,15 +4279,15 @@ export default class salesInvoice extends React.PureComponent
                                                     {
                                                         if(pResult.split('|')[0] != 'ERR')
                                                         {
-                                                            var a = document.createElement('a');
-                                                            a.href = "data:application/pdf;base64," + pResult.split('|')[1];
-                                                            a.setAttribute('target', '_blank');
-                                                            a.click()
-                                                            // var mywindow = window.open('printview.html','_blank',"width=900,height=1000,left=500");      
-                                                            // mywindow.onload = function() 
-                                                            // { 
-                                                            //     mywindow.document.getElementById("view").innerHTML="<iframe src='data:application/pdf;base64," + pResult.split('|')[1] + "' type='application/pdf' width='100%' height='100%'></iframe>"      
-                                                            // } 
+                                                            // var a = document.createElement('a');
+                                                            // a.href = "data:application/pdf;base64," + pResult.split('|')[1];
+                                                            // a.setAttribute('target', '_blank');
+                                                            // a.click()
+                                                            var mywindow = window.open('printview.html','_blank',"width=900,height=1000,left=500");      
+                                                            mywindow.onload = function() 
+                                                            { 
+                                                                mywindow.document.getElementById("view").innerHTML="<iframe src='data:application/pdf;base64," + pResult.split('|')[1] + "' type='application/pdf' width='100%' height='100%'></iframe>"      
+                                                            } 
                                                             // let mywindow = window.open('','_blank',"width=900,height=1000,left=500");
                                                             // mywindow.document.write("<iframe src='data:application/pdf;base64," + pResult.split('|')[1] + "' type='application/pdf' default-src='self' width='100%' height='100%'></iframe>");
                                                         }
