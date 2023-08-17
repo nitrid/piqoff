9793d35547a28a1e7fa540eec6c1da8c31a4b862
recepkaraca1 <receeep7@gmail.com>
2023-08-17 13:52:15 +0300

    ..

--- www/src/tab/pages/sale.js
+++ www/src/tab/pages/sale.js
@@ -77,10 +77,10 @@ export default class Sale extends React.PureComponent
         let tmpQuery = 
         {
             query :"SELECT  GUID,CODE,NAME,VAT,PRICE,IMAGE,UNIT,UNIT_NAME,UNIT_FACTOR FROM ITEMS_VW_02 " +
-            "WHERE STATUS = 1 AND ((UPPER(CODE) LIKE UPPER(@VAL + '%')) OR (UPPER(NAME) LIKE UPPER(@VAL + '%'))) AND " +
+            "WHERE STATUS = 1 AND ((UPPER(CODE) LIKE UPPER('%' + @VAL + '%')) OR (UPPER(NAME) LIKE UPPER('%' + @VAL + '%'))) AND " +
             "((MAIN_GRP = @MAIN_GRP) OR (@MAIN_GRP = '')) ORDER BY NAME",
             param : ['VAL:string|50','MAIN_GRP:string|50'],
-            value : [this.txtSearch.value,this.cmbGroup.value],
+            value : [this.txtSearch.value.replaceAll(' ','%'),this.cmbGroup.value],
             buffer : true
         }
         let tmpBuf = await this.core.sql.execute(tmpQuery) 
