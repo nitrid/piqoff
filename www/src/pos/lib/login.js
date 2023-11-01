6003ba210578518697bfbe7afcb804f29aa5d58d
recepkaraca1 <50820975+recepkaraca1@users.noreply.github.com>
2023-11-01 21:35:55 +0100

    .

--- www/src/pos/lib/login.js
+++ www/src/pos/lib/login.js
@@ -119,7 +119,8 @@ export default class Login extends React.Component
         {
             if(tmpData[i].CARDID == pValue)
             {
-                if(await this.core.auth.login(tmpData[i].CODE,tmpData[i].PWD,'OFF'))
+                console.log(tmpData[i].CARDID + " - " + pValue + " - " + tmpData[i].CODE + " - " + tmpData[i].PWD)
+                if(await this.core.auth.login(tmpData[i].CODE,tmpData[i].PWD,'POS'))
                 {
                     App.instance.setState({logined:true});
                     idCheck = true
