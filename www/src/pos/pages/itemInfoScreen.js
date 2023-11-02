3a68dced711a725e64b4086bcab6c4a59f22f5fb
piqpos <piqposfr@gmail.com>
2023-11-02 17:35:52 +0100

    ....

--- www/src/pos/pages/itemInfoScreen.js
+++ www/src/pos/pages/itemInfoScreen.js
@@ -6,7 +6,9 @@ import NbLabel from "../../core/react/bootstrap/label.js";
 import NdGrid,{Column,Editing,Paging,Scrolling} from "../../core/react/devex/grid.js";
 
 import { dataset,datatable,param,access } from "../../core/core.js";
-import {prm} from '../meta/prm.js'
+import {prm} from '../meta/prm.js';
+import '../css/itemInfoScreen.css'
+
 
 export default class itemInfoScreen extends React.PureComponent
 {
@@ -27,13 +29,41 @@ export default class itemInfoScreen extends React.PureComponent
     render()
     {
         return (
-            <div style={{backgroundColor:"#bdc3c7"}}>
+            <div>
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-6">
                         <img src="./css/img/piqlogo.png" />
                     </div>
+                    <div className="col-6 px-5">
+                        <img src="pos\resources\logoProInter.png" className="float-end" alt="Logo"/>
+                    </div>
                 </div>
-            </div> 
+                <div className="row">
+                    <div className="col-12">
+                        <div className="infoContainer">
+                            <div className="row">
+                                <div className="col-4">
+                                    <div className="productImg">
+                                        IMAGE:
+                                    </div>                    
+                                    <div className="productCodeBarre">
+                                        CODEBARRE:
+                                    </div>
+                                </div>
+                                <div className="col-8">
+                                    <div className="productName">
+                                        NAME:
+                                    </div>
+                                    <div className="productData">
+                                        DATA
+                                    </div>                      
+                                </div>
+                            </div>
+                            <button type="button" className="btn btn-success">VALIDER</button>
+                        </div>              
+                    </div>
+                </div>
+            </div>
         )
     }
 }
