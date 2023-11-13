f1d737b8ded947d6f3381595ff9c6f2d2ef4710e
nitrid <alikemalkaraca@gmail.com>
2023-11-13 20:27:05 +0100

    .

--- www/src/admin/pages/parameters/systemParamPos.js
+++ www/src/admin/pages/parameters/systemParamPos.js
@@ -9,7 +9,9 @@ import NdButton from '../../../core/react/devex/button';
 import {ItemBuild,ItemSet,ItemGet} from '../../tools/itemOp';
 import NdPopGrid from '../../../core/react/devex/popgrid';
 import NdGrid,{Column,Editing,Popup,Paging,Scrolling,KeyboardNavigation,Lookup} from '../../../core/react/devex/grid';
+import NdDialog, { dialog } from '../../../core/react/devex/dialog.js';
 import NdTextArea from '../../../core/react/devex/textarea';
+import ScrollView from 'devextreme-react/scroll-view';
 
 export default class systemParamPos extends React.PureComponent
 {
@@ -26,6 +28,7 @@ export default class systemParamPos extends React.PureComponent
         this.ItemGet = ItemGet.bind(this)
 
         this.prmData = null
+        this.loading = React.createRef();
     }
     async componentDidMount()
     {
@@ -53,95 +56,115 @@ export default class systemParamPos extends React.PureComponent
     {
         return(
             <div>
-                <div className='row px-2 pt-2'>
-                    <div className='col-12'>
-                        <Form colCount={2} id={"frmFilter" + this.tabIndex}>
-                            <Item>
-                                <Label text={this.t("lblUser")} alignment="right" />
-                                <NdSelectBox simple={true} parent={this} id="cmbUser" tabIndex={this.tabIndex}
-                                displayExpr="CODE"                       
-                                valueExpr="CODE"
-                                value={""}
-                                showClearButton={true}
-                                data={{source:{select:{query : "SELECT CODE,NAME FROM USERS ORDER BY NAME ASC"},sql:this.core.sql}}}
-                                onValueChanged={async(e)=>
-                                {
-                                    await this.prmData.load({APP:'POS'})
-                                    this.prmData.filter({TYPE:0}).meta.map((pItem) => 
+                <ScrollView>
+                    <div className='row px-2 pt-2'>
+                        <div className='col-12'>
+                            <Form colCount={2} id={"frmFilter" + this.tabIndex}>
+                                <Item>
+                                    <Label text={this.t("lblUser")} alignment="right" />
+                                    <NdSelectBox simple={true} parent={this} id="cmbUser" tabIndex={this.tabIndex}
+                                    displayExpr="CODE"                       
+                                    valueExpr="CODE"
+                                    value={""}
+                                    showClearButton={true}
+                                    data={{source:{select:{query : "SELECT CODE,NAME FROM USERS ORDER BY NAME ASC"},sql:this.core.sql}}}
+                                    onValueChanged={async(e)=>
                                     {
-                                        let tmpData = {...pItem} 
-                                        tmpData.VALUE = this.prmData.filter({TYPE:0,USERS:e.value,ID:pItem.ID}).getValue()                                        
-                                        this.ItemSet(tmpData)
-                                    })
-                                }}
-                                />
-                            </Item>
-                            <Item>
-                                <NdButton text={this.t("btnSave")} type="default" width="100%"
-                                onClick={async()=>
-                                {
-                                    this.pg_UserList.show();
-                                    this.pg_UserList.onClick = async(data) =>
-                                    {
-                                        for (let i = 0; i < data.length; i++) 
-                                        {
-                                            for (let x = 0; x < this.state.metaPrm.length; x++) 
-                                            {
-                                                this.prmData.add
-                                                (
-                                                    {
-                                                        TYPE:this.state.metaPrm[x].TYPE,
-                                                        ID:this.state.metaPrm[x].ID,
-                                                        VALUE:await this.ItemGet(this.state.metaPrm[x]),
-                                                        SPECIAL:this.state.metaPrm[x].SPECIAL,
-                                                        USERS:data[i].CODE,
-                                                        PAGE:this.state.metaPrm[x].PAGE,
-                                                        ELEMENT:this.state.metaPrm[x].ELEMENT,
-                                                        APP:this.state.metaPrm[x].APP,
-                                                    }
-                                                )
-                                            }
-                                        }
-                                        await this.prmData.save()
                                         await this.prmData.load({APP:'POS'})
-                                    }
-                                }}></NdButton>
-                            </Item>
-                        </Form>
-                    </div>    
-                </div>
-                <div className='row px-2 pt-2'>
-                    <div className='col-12'>                            
-                        <Form colCount={2} id={"frmParam" + this.tabIndex}>
-                            {this.buildItem()}
-                        </Form>  
+                                        this.prmData.filter({TYPE:0}).meta.map((pItem) => 
+                                        {
+                                            let tmpData = {...pItem}
+                                            tmpData.VALUE = this.prmData.filter({TYPE:0,USERS:e.value,ID:pItem.ID}).getValue()
+                                            this.ItemSet(tmpData)
+                                        })
+                                    }}
+                                    />
+                                </Item>
+                                <Item>
+                                    <NdButton text={this.t("btnSave")} type="default" width="100%"
+                                    onClick={async()=>
+                                    {
+                                        this.pg_UserList.show();
+                                        this.pg_UserList.onClick = async(data) =>
+                                        {
+                                            let tmpConfObj =
+                                            {
+                                                id:'msgSaveResult',showTitle:true,title:App.instance.lang.t("msgSaveResult.title"),showCloseButton:true,width:'500px',height:'200px',
+                                                button:[{id:"btn02",caption:App.instance.lang.t("msgSaveResult.btn01"),location:'after'}],
+                                            }
+
+                                            App.instance.setState({isExecute:true})
+                                            for (let i = 0; i < data.length; i++) 
+                                            {
+                                                for (let x = 0; x < this.state.metaPrm.length; x++) 
+                                                {
+                                                    this.prmData.add
+                                                    (
+                                                        {
+                                                            TYPE:this.state.metaPrm[x].TYPE,
+                                                            ID:this.state.metaPrm[x].ID,
+                                                            VALUE:await this.ItemGet(this.state.metaPrm[x]),
+                                                            SPECIAL:this.state.metaPrm[x].SPECIAL,
+                                                            USERS:data[i].CODE,
+                                                            PAGE:this.state.metaPrm[x].PAGE,
+                                                            ELEMENT:this.state.metaPrm[x].ELEMENT,
+                                                            APP:this.state.metaPrm[x].APP,
+                                                        }
+                                                    )
+                                                }
+                                            }
+                                            let tmpResult = await this.prmData.save()                                            
+                                            await this.prmData.load({APP:'POS'})
+                                            App.instance.setState({isExecute:false})
+
+                                            if(tmpResult == 0)
+                                            {
+                                                tmpConfObj.content = (<div style={{textAlign:"center",fontSize:"20px"}}>{App.instance.lang.t("msgSaveResult.msgSuccess")}</div>)
+                                            }
+                                            else
+                                            {
+                                                tmpConfObj.content = (<div style={{textAlign:"center",fontSize:"20px",color:"red"}}>{App.instance.lang.t("msgSaveResult.msgFailed")}</div>)
+                                            }
+                                            await dialog(tmpConfObj);
+                                        }
+                                    }}></NdButton>
+                                </Item>
+                            </Form>
+                        </div>    
                     </div>
-                </div>
-                <div>
-                    <NdPopGrid id={"pg_UserList"} parent={this} container={"#root"}
-                    visible={false}
-                    position={{of:'#root'}}
-                    showTitle={true}
-                    showBorders={true}
-                    width={'90%'}
-                    height={'90%'}
-                    title={this.t("pg_UserList.title")}
-                    data =
-                    {{
-                        source:
-                        {
-                            select:
+                    <div className='row px-2 pt-2'>
+                        <div className='col-12'>                            
+                            <Form colCount={2} id={"frmParam" + this.tabIndex}>
+                                {this.buildItem()}
+                            </Form>  
+                        </div>
+                    </div>
+                    <div>
+                        <NdPopGrid id={"pg_UserList"} parent={this} container={"#root"}
+                        visible={false}
+                        position={{of:'#root'}}
+                        showTitle={true}
+                        showBorders={true}
+                        width={'90%'}
+                        height={'90%'}
+                        title={this.t("pg_UserList.title")}
+                        data =
+                        {{
+                            source:
                             {
-                                query : "SELECT CODE,NAME FROM USERS ORDER BY CODE ASC"
-                            },
-                            sql:this.core.sql
-                        }
-                    }}
-                    >
-                        <Column dataField="CODE" caption={this.t("pg_UserList.clmCode")} width={300} />
-                        <Column dataField="NAME" caption={this.t("pg_UserList.clmName")} width={300} defaultSortOrder="asc" />
-                    </NdPopGrid>
-                </div>
+                                select:
+                                {
+                                    query : "SELECT CODE,NAME FROM USERS ORDER BY CODE ASC"
+                                },
+                                sql:this.core.sql
+                            }
+                        }}
+                        >
+                            <Column dataField="CODE" caption={this.t("pg_UserList.clmCode")} width={300} />
+                            <Column dataField="NAME" caption={this.t("pg_UserList.clmName")} width={300} defaultSortOrder="asc" />
+                        </NdPopGrid>
+                    </div>
+                </ScrollView>
             </div>
         )
     }
