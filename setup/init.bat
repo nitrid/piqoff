0b6d8ee7a32f7f02f932df94f689eb13d836196b
nitrid <alikemalkaraca@gmail.com>
2024-03-11 10:01:37 +0100

    .

--- setup/init.bat
+++ setup/init.bat
@@ -1,13 +1,14 @@
 @echo off
-echo Cloning the Piqoff repository...
-git clone https://github.com/nitrid/piqoff
-cd piqoff
+SET repoDir=C:\tst
+echo Cloning the Piqoff repository into %repoDir%...
+IF NOT EXIST "%repoDir%" mkdir "%repoDir%"
+cd /d "%repoDir%"
+git clone https://github.com/nitrid/piqoff .
 echo Installing dependencies for Piqoff...
 npm install
 cd www
 echo Installing dependencies for the frontend...
 npm install
-cd ..
 cd setup
 echo Running setup script...
 node init.js
