537de3344e35486fa7f647040972c5d07b62f401
nitrid <alikemalkaraca@gmail.com>
2024-03-31 16:14:34 +0200

    .

--- www/src/pos/plugins/wgtPersonTrack/meta/personPrint.js
+++ www/src/pos/plugins/wgtPersonTrack/meta/personPrint.js
@@ -28,12 +28,15 @@ export function print()
         {font:"a",style:"b",align:"ct",data: data.firm.length > 0 ? "Siret " + data.firm[0].SIRET_ID + " - APE " + data.firm[0].APE_CODE : "Siret 94 929 096 900 011 - APE 6201Z"},
         {font:"a",style:"b",align:"ct",data: data.firm.length > 0 ? "Nr. TVA " + data.firm[0].INT_VAT_NO : "Nr. TVA FR61949290969"},
         {font:"a",style:"b",size : [1,1],align:"ct",data:""},
-        {font:"a",style:"b",size : [1,0.5],align:"ct",data: "INFORMATIONS DE L'UTILISATEUR"},
+        {font:"a",style:"b",size : [0.5,0.5],align:"ct",data: "INFORMATIONS DE L'UTILISATEUR"},
         {font:"b",style:"b",align:"lt",data:"****************************************************************"},
         {font:"a",align:"lt",data:moment(new Date().toISOString()).utcOffset(0,true).locale('fr').format('dddd DD.MM.YYYY HH:mm:ss')},
         {font:"a",align:"lt",pdf:{fontSize:11},data:("Utilisateur: " + data.special.user).space(25,'e') + ("Caisse: " + data.special.device).space(23,'s')},
-        {font:"a",style:"b",size : [1,0.5],align:"ct",data: data.special.description.space(48)},
+        {font:"a",style:"b",align:"lt",data:" ".space(48)},
+        {font:"a",style:"b",size : [0.5,0.5],align:"ct",data: data.special.description.space(48)},
         {font:"b",style:"b",align:"lt",data:"****************************************************************"},        
+        {font:"a",style:"b",align:"lt",data:" ".space(48)},
+        {font:"a",style:"b",align:"lt",data:" ".space(48)},
         {font:"a",style:"b",align:"lt",data:" ".space(48)}
     ]
 }
