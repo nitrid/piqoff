129128df0d7bf5d4742104ea7bf80ea2a39ca3b0
nitrid <alikemalkaraca@gmail.com>
2024-05-03 19:52:42 +0200

    .

---
+++ www/src/off/meta/lang/tr/slsRpt_01_009.js
@@ -0,0 +1,26 @@
+// "Açık Satış Fatura Raporu"
+const slsRpt_01_009 =
+{
+    btnGet: "Getir",
+    txtCustomerCode : "Müşteri", 
+    grdListe : 
+    {
+        clmDate : "Tarih",
+        clmCode: "Kodu",
+        clmName : "Adı",           
+        clmAmount : "Tutar",
+        clmRefNo: "Sıra",
+        clmRemainder : "Kalan",
+        clmTotal : "Toplam",
+        clmRef : "Seri-Sıra"
+    },
+    pg_txtCustomerCode : 
+    {
+        title : "Müşteri Seçimi",
+        clmCode :  "CARİ KODU",
+        clmTitle : "CARİ ADI",
+        clmTypeName : "TİPİ",
+        clmGenusName : "CİNSİ"
+    },
+}
+export default slsRpt_01_009
