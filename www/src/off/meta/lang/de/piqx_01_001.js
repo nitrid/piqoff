5f7b24cef61207517a918720dd489880995349ca
nitrid <alikemalkaraca@gmail.com>
2024-05-10 13:07:10 +0200

    PiqX fatura import işlemi yeniden düzenlendi.

--- www/src/off/meta/lang/de/piqx_01_001.js
+++ www/src/off/meta/lang/de/piqx_01_001.js
@@ -11,37 +11,12 @@ const piqx_01_001 =
         clmFromTitle: "Bezeichnung",
         clmStatus: "Status"
     },
-    popImport:
+    msgImport:
     {
-        title: "Importieren",
-        btnImport: "Importieren",
-        txtCustomerCode: "Lieferantencode",
-        txtCustomerName: "Lieferantenname",
-        dtDocDate: "Belegdatum",
-        dtShipDate: "Versanddatum",
-        cmbDepot: "Lager",
-        txtHT: "Zwischensumme",
-        txtTax: "Steuer",
-        txtTTC: "Gesamtsumme",
-        clmItemCode: "Artikelcode",
-        clmMulticode: "Mehrfachcode",
-        clmItemName: "Artikelname",
-        clmQuantity: "Menge",
-        clmPrice: "Preis",
-        clmDiscount: "Rabatt",
-        clmAmount: "Betrag",
-        msgImport:
-        {
-            title: "Achtung",
-            btn01: "OK",
-            msg1: "Keine Daten zu importieren!",
-            msg2: "Bitte wählen Sie ein Lager aus!",
-            msg3: "Import kann nicht durchgeführt werden, Lieferant nicht gefunden!",
-            msg4: "Sie können die Übertragung nicht durchführen, solange nicht definierte Produkte in der Liste sind!",
-            msg5: "Die Einkaufsrechnung wurde unter der Nummer registriert.",
-            msg6: "Sie können eine bereits importierte Rechnung nicht erneut registrieren!",
-        }
-    },
+        title: "Achtung",
+        btn01: "OK",
+        msg: "Sie können eine bereits importierte Rechnung nicht erneut registrieren!",
+    }
 }
 
 export default piqx_01_001;
--- www/src/off/meta/lang/fr/piqx_01_001.js
+++ www/src/off/meta/lang/fr/piqx_01_001.js
@@ -11,37 +11,12 @@ const piqx_01_001 = //BAK
         clmFromTitle: "Dénomination",
         clmStatus: "Statut"
     },
-    popImport:
+    msgImport:
     {
-        title: "Importer",
-        btnImport: "Importer",
-        txtCustomerCode: "Code fournisseur",
-        txtCustomerName: "Nom du fournisseur",
-        dtDocDate: "Date du document",
-        dtShipDate: "Date d'expédition",
-        cmbDepot: "Dépôt",
-        txtHT: "Total HT",
-        txtTax: "Taxes",
-        txtTTC: "Total TTC",
-        clmItemCode: "Code article",
-        clmMulticode: "Code multiple",
-        clmItemName: "Nom de l'article",
-        clmQuantity: "Quantité",
-        clmPrice: "Prix",
-        clmDiscount: "Remise",
-        clmAmount: "Montant",
-        msgImport:
-        {
-            title: "Attention",
-            btn01: "OK",
-            msg1: "Aucune donnée à importer !",
-            msg2: "Veuillez sélectionner un dépôt !",
-            msg3: "Impossible de réaliser l'importation, fournisseur introuvable !",
-            msg4: "Vous ne pouvez pas effectuer le transfert tant que la liste contient des articles non définis !",
-            msg5: "La facture d'achat a été enregistrée sous le numéro.",
-            msg6: "Vous ne pouvez pas enregistrer à nouveau une facture déjà importée !",
-        }
-    },
+        title: "Attention",
+        btn01: "OK",
+        msg: "Vous ne pouvez pas enregistrer à nouveau une facture déjà importée !",
+    }
 }
 
 export default piqx_01_001;
