e44a4cf15e86ca02e5c1520cd64940e0b4b99451
Burak <50820975+recepkaraca1@users.noreply.github.com>
2024-05-30 16:03:37 +0200

    Create pos_02_010.js
    turkce dil sayfası olusturuldu

---
+++ www/src/off/meta/lang/tr/pos_02_010.js
@@ -0,0 +1,17 @@
+// "Ürün Detaylı Satış Raporu"
+const pos_02_010 =
+{
+    chkItemCreated: "Ürünlerin oluşturulma tarihine göre geri almak",
+    btnGet :"Getir", 
+    grdListe : 
+    {
+        clmDate: "Tarih", 
+        clmCode: "Kodu", 
+        clmName : "Adı",             
+        clmQuantity : "Miktar",  
+        clmAmount : "Toplam", 
+        clmVat : "Fiş",  
+        clmTotal : "Toplam Tutar",  
+    },
+}
+export default pos_02_010
