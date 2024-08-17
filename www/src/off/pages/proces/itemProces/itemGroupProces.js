82e80666e9e778fadb72d20beb23ea063cc36058
recepkaraca1 <50820975+recepkaraca1@users.noreply.github.com>
2024-08-17 14:49:46 +0200

    1.0.12
    -  Toplu ürün grubu güncelleme düzeltildi

--- www/src/off/pages/proces/itemProces/itemGroupProces.js
+++ www/src/off/pages/proces/itemProces/itemGroupProces.js
@@ -69,13 +69,14 @@ export default class itemList extends React.PureComponent
 
         for (let i = 0; i < this.grdListe.getSelectedData().length; i++) 
         {
+            console.log(this.cmbItemGrp.value + '---' +this.grdListe.getSelectedData()[i].GUID)
             let tmpQuery = 
             {
                 query :"UPDATE ITEMS_GRP SET MAIN_GUID = @MAIN WHERE ITEM = @ITEM ",
-                param : ['MAIN:string|25','ITEM:string|50'],
+                param : ['MAIN:string|50','ITEM:string|50'],
                 value : [this.cmbItemGrp.value,this.grdListe.getSelectedData()[i].GUID]
             }
-            let tmpData = await this.core.sql.execute(tmpQuery) 
+            await this.core.sql.execute(tmpQuery) 
         }
         let tmpConfObj1 =
         {
