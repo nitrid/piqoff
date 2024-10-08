ca495623511c8b64e36f4c11d54b18f0b0c84f52
recepkaraca1 <50820975+recepkaraca1@users.noreply.github.com>
2024-10-08 18:22:06 +0200

    ..

--- plugins/piqService.js
+++ plugins/piqService.js
@@ -1,22 +1,43 @@
 import {core} from 'gensrv'
+import moment from 'moment'
+import * as xlsx from 'xlsx'
+import fs from 'fs'
+import rsa from 'jsrsasign'
+import { pem } from '../pem.js'
 import cron from 'node-cron';
-
-class customerpoint
+import nodemailer from  'nodemailer'
+class mailer
 {
     constructor()
     {
+        this.socket = null;
         this.core = core.instance;
         this.connEvt = this.connEvt.bind(this)
         this.core.socket.on('connection',this.connEvt)
 
         this.processRun()
     }
+
     connEvt(pSocket)
-    {       
-        
+    {
+        // Mail Gönderimi
+        pSocket.on('mailer',async (pParam,pCallback) =>
+        {
+            pCallback(await this.mailSend(pParam))
+        })
+        // Kasalara mesaj gönderimi
+        pSocket.on('msgService',(pParam,pCallback) =>
+        {
+            if(pParam.tag == 'msgPosDevice')
+            {
+                this.core.socket.emit('msgService',pParam)
+            }
+        })
     }
+
     async processRun()
     {
+        // Şubat ayında Müşteri puan sıfırlaması 
         cron.schedule('0 1 1 2 *', async () => 
         {
             try
@@ -85,7 +106,115 @@ class customerpoint
                 console.log(err);
             }
         });
+        // Depo miktarlarının Güncellenmesi
+        cron.schedule('0 3 * * *', async () => 
+        {
+            tmpQuery = 
+            {
+                query : "SELECT * FROM ITEM_QUANTITY ",
+            }
+       
+            let tmpResult = (await core.instance.sql.execute(tmpQuery)).result.recordset
+            for (let i = 0; i < tmpResult.length; i++) 
+            {
+                
+                let tmpUpdateQuery = 
+                {
+                    query : "EXEC CUSTOMERS SET POINT = (SELECT dbo.FN_CUSTOMER_TOTAL_POINT(GUID,GETDATE())) WHERE GUID = @CUSTOMER", 
+                    param : ['CUSTOMER:string|50'],
+                    value : [tmpResultCustomer.result.recordset[i].GUID],
+                }
+                await core.instance.sql.execute(tmpUpdateQuery)
+            }
         
+        });
+    }
+
+    async mailSend(pData)
+    {
+        return new Promise(async resolve =>
+        {
+            let tmpAttach = [];
+            if(typeof pData.attachName != 'undefined')
+            {
+                tmpAttach = 
+                [
+                    {
+                        filename: pData.attachName,
+                        content: pData.attachData,
+                        encoding: 'base64'
+                    }
+                ]
+            }
+
+            let tmpQuery
+            if(typeof pData.mailGuid != 'undefined' || pData.mailGuid == '')
+            {
+                tmpQuery = 
+                {
+                    query : "SELECT * FROM MAIL_SETTINGS WHERE GUID = @GUID ",
+                    param : ['GUID:string|50'],
+                    value : [pData.mailGuid]
+                }
+            }
+            else
+            {
+                tmpQuery = 
+                {
+                    query : "SELECT * FROM MAIL_SETTINGS WHERE MASTER = 1 ",
+                }
+            }
+           
+
+            let tmpResult = (await core.instance.sql.execute(tmpQuery)).result.recordset
+            if(typeof tmpResult[0] != 'undefined')
+            {
+                let tmpservice = ''
+                if(tmpResult[0].MAIL_SERVICE == 'gmail')
+                {
+                    tmpservice = 'gmail'
+                }
+                let transporter = nodemailer.createTransport(
+                {
+                    service: tmpservice,
+                    host: tmpResult[0].MAIL_SMTP,
+                    port: tmpResult[0].MAIL_PORT,
+                    secure: true,
+                    auth: 
+                    {
+                    user: tmpResult[0].MAIL_ADDRESS,
+                    pass: tmpResult[0].MAIL_PASSWORD
+                    },
+                    //tls : { rejectUnauthorized: false }
+                });
+                var mailOptions = 
+                {
+                    from: tmpResult[0].MAIL_ADDRESS,
+                    to: pData.sendMail,
+                    subject: pData.subject,
+                    html:pData.html,
+                    text:pData.text,
+                    attachments: tmpAttach
+                };
+                transporter.sendMail(mailOptions, function(error, info)
+                {
+                    if (error) 
+                    {
+                        console.log(error)
+                        resolve(error);
+                    }
+                    else
+                    {
+                        resolve(0);
+                    }
+                });
+            }
+            else
+            {
+                resolve(1);
+            }
+        })
     }
 }
-export const _customerpoint = new customerpoint()
+
+export const _mailer = new mailer()
