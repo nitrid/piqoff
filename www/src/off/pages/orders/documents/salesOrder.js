708e655f5789aa4ae3a73ad70ccfcad84c38e13e
recepkaraca1 <50820975+recepkaraca1@users.noreply.github.com>
2025-01-24 14:35:32 +0100

    ...

--- www/src/off/pages/orders/documents/salesOrder.js
+++ www/src/off/pages/orders/documents/salesOrder.js
@@ -1091,6 +1091,10 @@ export default class salesOrder extends DocBase
                                     data = {{source:{select:{query : "SELECT NO,NAME FROM ITEM_PRICE_LIST_VW_01 ORDER BY NO ASC"},sql:this.core.sql}}}
                                     param = {this.param.filter({ELEMENT:'cmbPricingList',USERS:this.user.CODE})}
                                     access = {this.access.filter({ELEMENT:'cmbPricingList',USERS:this.user.CODE})}
+                                    onValueChanged={(async()=>
+                                        {
+                                            this.priceListChange()
+                                        }).bind(this)}
                                     >
                                     </NdSelectBox>
                                 </Item>
