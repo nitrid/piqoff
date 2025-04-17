9ccd61b18f369056d7b81b35577356803a16c5ff
Ceyhun Sahin <67160612+ceyhunsahin@users.noreply.github.com>
2025-04-17 15:41:10 +0200

    Menüde "Fiyat Listesi" seçeneği eklendi ve dil dosyalarında gerekli çeviriler yapıldı. Ayrıca, fiyat listesi verileri için yeni sorgular ve bileşenler eklendi.

--- www/src/off/pages/items/lists/priceList.js
+++ www/src/off/pages/items/lists/priceList.js
@@ -6,136 +6,69 @@ import Toolbar,{Item} from 'devextreme-react/toolbar';
 import Form, { Label } from 'devextreme-react/form';
 import ScrollView from 'devextreme-react/scroll-view';
 
-import NdGrid,{Column, ColumnChooser,ColumnFixing,Paging,Pager,Scrolling,Export} from '../../../../core/react/devex/grid.js';
+import NdGrid,{Column, ColumnChooser,ColumnFixing,Paging,Pager,Scrolling,Export,Editing,StateStoring} from '../../../../core/react/devex/grid.js';
 import NdTextBox from '../../../../core/react/devex/textbox.js'
 import NdSelectBox from '../../../../core/react/devex/selectbox.js';
 import NdDropDownBox from '../../../../core/react/devex/dropdownbox.js';
 import NdListBox from '../../../../core/react/devex/listbox.js';
 import NdButton from '../../../../core/react/devex/button.js';
 import NdCheckBox from '../../../../core/react/devex/checkbox.js';
+import NdTagBox from '../../../../core/react/devex/tagbox.js';
 import { dialog } from '../../../../core/react/devex/dialog.js';
+import DocBase from '../../../tools/DocBase.js';
 
-export default class itemList extends React.PureComponent
+
+export default class priceList extends DocBase
 {
     constructor(props)
     {
         super(props)
-
-        this.state = 
-        {
-            columnListValue : ['ITEM_CODE','ITEM_NAME','CUSTOMER_NAME','PRICE','MAIN_GRP_NAME']
-        }
-        
+        this.prmObj = this.param.filter({TYPE:1,USERS:this.user.CODE});
         this.core = App.instance.core;
-        this.columnListData = 
-        [
-            {CODE : "ITEM_CODE",NAME : this.t("grdListe.clmCode")},
-            {CODE : "ITEM_NAME",NAME : this.t("grdListe.clmName")},                                   
-            {CODE : "CUSTOMER_NAME",NAME : this.t("grdListe.clmCustomer")},
-            {CODE : "PRICE",NAME : this.t("grdListe.clmPrice")},    
-            {CODE : "MAIN_GRP_NAME",NAME : this.t("grdListe.clmMainGrp")},
-            {CODE : "QUANTITY",NAME : this.t("grdListe.clmQuantity")},
-            {CODE : "CHANGE",NAME :this.t("grdListe.clmChange")},
-            {CODE : "START",NAME : this.t("grdListe.clmStartDate")},
-            {CODE : "FINISH",NAME : this.t("grdListe.clmFinishDate")}
-        ]
         this.groupList = [];
         this._btnGetirClick = this._btnGetirClick.bind(this)
-        this._columnListBox = this._columnListBox.bind(this)
+        this.saveState = this.saveState.bind(this)
+        this.loadState = this.loadState.bind(this)
+    }
+    loadState() 
+    {
+        let tmpLoad = this.access.filter({ELEMENT:'grdPriceListe',USERS:this.user.CODE})
+        return tmpLoad.getValue()
+    }
+    saveState(e)
+    {
+        let tmpSave = this.access.filter({ELEMENT:'grdPriceListe',USERS:this.user.CODE})
+        tmpSave.setValue(e)
+        tmpSave.save()
     }
     componentDidMount()
     {
-        setTimeout(async () => 
-        {
-            // this.test.data.source.groupBy = ["NAME"]
-            // await this.test.dataRefresh()
-            //console.log(this.test.data.datatable)
-            // this.test.data.store.load().then(
-            //     (data) => { console.log(data)},
-            //     (error) => { /* Handle the "error" here */ }
-            // );
-        }, 1000);
-    }
-    _columnListBox(e)
-    {
-        let onOptionChanged = (e) =>
-        {
-            if (e.name == 'selectedItemKeys') 
-            {
-                this.groupList = [];
-                if(typeof e.value.find(x => x == 'MULTICODE') != 'undefined')
-                {
-                    this.groupList.push('MULTICODE')
-                }
-                if(typeof e.value.find(x => x == 'BARCODE') != 'undefined')
-                {
-                    this.groupList.push('BARCODE')
-                }                
-                if(typeof e.value.find(x => x == 'CODE') != 'undefined')
-                {
-                    this.groupList.push('CODE')
-                }
-                
-                for (let i = 0; i < this.grdListe.devGrid.columnCount(); i++) 
-                {
-                    if(typeof e.value.find(x => x == this.grdListe.devGrid.columnOption(i).name) == 'undefined')
-                    {
-                        this.grdListe.devGrid.columnOption(i,'visible',false)
-                    }
-                    else
-                    {
-                        this.grdListe.devGrid.columnOption(i,'visible',true)
-                    }
-                }
-
-                this.setState(
-                    {
-                        columnListValue : e.value
-                    }
-                )
-            }
-        }
-        
-        return(
-            <NdListBox id='columnListBox' parent={this}
-            data={{source: this.columnListData}}
-            width={'100%'}
-            showSelectionControls={true}
-            selectionMode={'multiple'}
-            displayExpr={'NAME'}
-            keyExpr={'CODE'}
-            value={this.state.columnListValue}
-            onOptionChanged={onOptionChanged}
-            >
-            </NdListBox>
-        )
     }
     async _btnGetirClick()
     {
+        if(this.cmbPricingList.value == '')
+        {
+            App.instance.alert(this.lang.t("msgWarning"),"error")
+            return;
+        }
         let tmpSource =
         {
             source : 
             {
-                groupBy : this.groupList,
                 select : 
                 {
-                    query : "SELECT *,CONVERT(VARCHAR,CHANGE_DATE,104) AS CHANGE, " +
-                            "CONVERT(VARCHAR,START_DATE,104) AS START,CONVERT(VARCHAR,FINISH_DATE,104) AS FINISH " +
-                            "FROM ITEM_PRICE_VW_01 " +
-                            "WHERE  " +
-                            "((ITEM_NAME like @ITEM_NAME + '%') OR (@ITEM_NAME = '')) AND " +
-                            "((MAIN_GRP = @MAIN_GRP) OR (@MAIN_GRP = '')) AND " +
-                            "((CUSTOMER_CODE = @CUSTOMER_CODE) OR (@CUSTOMER_CODE = ''))",
-                    param : ['ITEM_NAME:string|250','MAIN_GRP:string|25','CUSTOMER_CODE:string|25'],
-                    value : [this.txtUrunAdi.value,this.cmbUrunGrup.value,this.cmbTedarikci.value]
+                    query : "SELECT * FROM ITEM_PRICE_VW_01 WHERE LIST_NO = @LIST_NO AND CUSTOMER_GUID = '00000000-0000-0000-0000-000000000000' AND TYPE = 0",
+                    param : ['LIST_NO:int'],
+                    value : [this.cmbPricingList.value]
                 },
                 sql : this.core.sql
             }
         }
-        
+
         App.instance.setState({isExecute:true})
-        await this.grdListe.dataRefresh(tmpSource)
+        await this.grdPriceListe.dataRefresh(tmpSource)
         App.instance.setState({isExecute:false})
+        this.txtTotalCount.value = this.grdPriceListe.data.datatable.length
     }
     render()
     {
@@ -157,9 +90,9 @@ export default class itemList extends React.PureComponent
                                         {
                                             App.instance.menuClick(
                                             {
-                                                id: 'stk_01_001',
-                                                text: 'Stok Tanımları',
-                                                path: 'items/cards/itemCard.js'
+                                                id: 'stk_03_002',
+                                                text: this.lang.t('menuOff.stk_03_002'),
+                                                path: 'items/lists/priceList.js',
                                             })
                                         }
                                     }    
@@ -196,51 +129,30 @@ export default class itemList extends React.PureComponent
                         <div className="col-12">
                             <Form colCount={2} id="frmKriter">
                                 <Item>
-                                    <Label text={this.t("cmbCustomer")} alignment="right" />
-                                        <NdSelectBox simple={true} parent={this} id="cmbTedarikci" showClearButton={true} notRefresh={true}  searchEnabled={true}
-                                        displayExpr="TITLE"                       
-                                        valueExpr="CODE"
-                                        data={{source: {select : {query:"SELECT CODE,TITLE FROM CUSTOMER_VW_01 WHERE TYPE IN(1,2) ORDER BY TITLE ASC"},sql : this.core.sql}}}
-                                        // onValueChanged={onValueChanged}
-                                        />
-                                </Item>
-                                <Item>
-                                    <Label text={this.t("txtItemName")} alignment="right" />
-                                        <NdTextBox id="txtUrunAdi" parent={this} simple={true} 
-                                        upper={this.sysParam.filter({ID:'onlyBigChar',USERS:this.user.CODE}).getValue().value}/>
-                                </Item>
-                                <Item>
-                                    <Label text={this.t("cmbMainGrp")} alignment="right" />
-                                        <NdSelectBox simple={true} parent={this} id="cmbUrunGrup" showClearButton={true} notRefresh={true}  searchEnabled={true}
-                                        displayExpr="NAME"                       
-                                        valueExpr="CODE"
-                                        data={{source: {select : {query:"SELECT CODE,NAME FROM ITEM_GROUP ORDER BY NAME ASC"},sql : this.core.sql}}}
-                                        // onValueChanged={onValueChanged}
-                                        />
-                                </Item>
-                                <Item> </Item>
-                                <Item>
-                                    <Label text={this.t("btnCheck")} alignment="right" />
-                                        <NdCheckBox id="chkAktif" parent={this} defaultValue={true}></NdCheckBox>
+                                <Label text={this.t("cmbPricingList")} alignment="right" />
+                                    <NdSelectBox simple={true} parent={this} id="cmbPricingList" notRefresh={true}
+                                    displayExpr = "NAME"
+                                    valueExpr = "NO"
+                                    value = ""
+                                    searchEnabled = {true}
+                                    data = {{source:{select:{query : "SELECT NO,NAME FROM ITEM_PRICE_LIST_VW_01 ORDER BY NO ASC"},sql:this.core.sql}}}
+                                    param = {this.param.filter({ELEMENT:'cmbPricingList',USERS:this.user.CODE})}
+                                    access = {this.access.filter({ELEMENT:'cmbPricingList',USERS:this.user.CODE})}
+
+                                    >
+                                    </NdSelectBox>
                                 </Item>
                             </Form>
                         </div>
                     </div>
                     <div className="row px-2 pt-2">
                         <div className="col-3">
-                            <NdDropDownBox simple={true} parent={this} id="cmbColumn"
-                            value={this.state.columnListValue}
-                            displayExpr="NAME"                       
-                            valueExpr="CODE"
-                            data={{source: this.columnListData}}
-                            contentRender={this._columnListBox}
-                            />
                         </div>
-                        <div className="col-3">
-                            
+                        <div className="col-2">
                         </div>
-                        <div className="col-3">
-                            
+                        <div className="col-2">
+                        </div>
+                        <div className="col-2">
                         </div>
                         <div className="col-3">
                             <NdButton text={this.t("btnGet")} type="success" width="100%" onClick={this._btnGetirClick}></NdButton>
@@ -248,7 +160,17 @@ export default class itemList extends React.PureComponent
                     </div>
                     <div className="row px-2 pt-2">
                         <div className="col-12">
-                            <NdGrid id="grdListe" parent={this} 
+                            <Form colCount={3} id="frmKriter">
+                                <Item>
+                                    <Label text={this.t("txtTotalCount")} alignment="right" />
+                                    <NdTextBox id="txtTotalCount" parent={this} simple={true} readOnly={true}/>
+                                </Item>
+                            </Form>
+                        </div>
+                    </div>
+                    <div className="row px-2 pt-2">
+                        <div className="col-12">
+                            <NdGrid id="grdPriceListe" parent={this} 
                             selection={{mode:"multiple"}} 
                             height={600}
                             showBorders={true}
@@ -258,22 +180,32 @@ export default class itemList extends React.PureComponent
                             allowColumnReordering={true}
                             allowColumnResizing={true}
                             loadPanel={{enabled:true}}
-                            >                            
-                                {this.sysParam.filter({ID:'pageListControl',USERS:this.user.CODE}).getValue().value == true ? <Paging defaultPageSize={20} /> : <Paging enabled={false} />}
-                                {this.sysParam.filter({ID:'pageListControl',USERS:this.user.CODE}).getValue().value == true ? <Pager visible={true} allowedPageSizes={[5,10,50]} showPageSizeSelector={true} /> : <Paging enabled={false} />}
-                                {this.sysParam.filter({ID:'pageListControl',USERS:this.user.CODE}).getValue().value == true ? <Scrolling mode="standart" /> : <Scrolling mode="infinite" />}
-                                <Export fileName={this.lang.t("menuOff.stk_03_002")} enabled={true} allowExportSelectedData={true} />
-                                <Column dataField="ITEM_CODE" caption={this.t("grdListe.clmCode")} visible={true}/> 
-                                <Column dataField="ITEM_NAME" caption={this.t("grdListe.clmName")} visible={true} defaultSortOrder="asc"/> 
-                                <Column dataField="MAIN_GRP_NAME" caption={this.t("grdListe.clmMainGrp")} visible={true}/> 
-                                <Column dataField="PRICE" caption={this.t("grdListe.clmPrice")} visible={true}/> 
-                                <Column dataField="CUSTOMER_NAME" caption={this.t("grdListe.clmCustomer")} visible={true}/> 
-                                <Column dataField="QUANTITY" caption={this.t("grdListe.clmQuantity")} visible={true}/> 
-                                <Column dataField="CHANGE" caption={this.t("grdListe.clmChange")} visible={false}/> 
-                                <Column dataField="START" caption={this.t("grdListe.clmStartDate")} visible={false}/> 
-                                <Column dataField="FINISH" caption={this.t("grdListe.clmFinishDate")} visible={false}/> 
-
-                               
+                            onRowDblClick={async(e)=>
+                            {
+                                App.instance.menuClick(
+                                    {
+                                        id: 'stk_01_001',
+                                        text: e.data.ITEM_NAME.substring(0,10),
+                                        path: 'items/cards/itemCard.js',
+                                        pagePrm:{CODE:e.data.ITEM_CODE}
+                            })
+                            }}
+                            >                                    
+                                <StateStoring enabled={true} type="custom" customLoad={this.loadState} customSave={this.saveState} storageKey={this.props.data.id + "_grdPriceListe"}/>
+                                <ColumnChooser enabled={true} />
+                                <Paging defaultPageSize={10} />
+                                <Pager visible={true} allowedPageSizes={[5,10,20,50,100]} showPageSizeSelector={true} />
+                                <Scrolling mode="standart" />
+                                <Editing mode="cell" allowUpdating={false} allowDeleting={false} confirmDelete={false}/>
+                                <Export fileName={this.lang.t("menuOff.stk_03_001")} enabled={true} allowExportSelectedData={true} />
+                                <Column dataField="LIST_NO" caption={this.t("grdPriceListe.clmListNo")} visible={true} width={80}/> 
+                                <Column dataField="LIST_NAME" caption={this.t("grdPriceListe.clmListName")} visible={true} defaultSortOrder="asc" width={160}/> 
+                                <Column dataField="ITEM_CODE" caption={this.t("grdPriceListe.clmItemCode")} visible={true} width={150}/> 
+                                <Column dataField="ITEM_NAME" caption={this.t("grdPriceListe.clmItemName")} visible={true} /> 
+                                <Column dataField="MAIN_GRP_NAME" caption={this.t("grdPriceListe.clmMainGrpName")} visible={true} width={150}/> 
+                                <Column dataField="QUANTITY" caption={this.t("grdPriceListe.clmQuantity")} visible={true} width={80}/> 
+                                <Column dataField="PRICE_HT" caption={this.t("grdPriceListe.clmPriceHt")} visible={true} format={{ style: "currency", currency: "EUR",precision: 2}} width={150}/> 
+                                <Column dataField="PRICE_TTC" caption={this.t("grdPriceListe.clmPriceTtc")} visible={true} format={{ style: "currency", currency: "EUR",precision: 2}} width={150}/>                         
                             </NdGrid>
                         </div>
                     </div>
