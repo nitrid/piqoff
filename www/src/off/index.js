69b0e008f727144a0afb9202cc139cd4d284d331
Ceyhun Sahin <67160612+ceyhunsahin@users.noreply.github.com>
2025-07-08 14:52:09 +0200

    index.js'e hasown property eklendi

--- www/src/off/index.js
+++ www/src/off/index.js
@@ -1,3 +1,10 @@
+// DevExtreme 25.x uyumluluğu için polyfill
+if (!Object.hasOwn) {
+    Object.hasOwn = function(obj, prop) {
+        return Object.prototype.hasOwnProperty.call(obj, prop);
+    };
+}
+
 import React from 'react';
 import App from './lib/app.js'
 import ReactDOM from 'react-dom';
