f95a8925fb3c6ce73236850f766deef653c84834
nitrid <alikemalkaraca@gmail.com>
2025-07-15 11:54:53 +0200

    1.0.19

--- www/src/off/pages/items/documents/itemOutDoc.js
+++ www/src/off/pages/items/documents/itemOutDoc.js
@@ -1353,8 +1353,7 @@ export default class itemEntryDoc extends React.PureComponent
                         </NdForm>
                     </NdPopUp>
                     {/* combineItem Dialog  */}
-                    <NdDialog id={"msgCombineItem"} container={'#' + this.props.data.id + this.tabIndex} parent={this}
-                    position={{at:'center center'}}
+                    <NdDialog id={"msgCombineItem"} container={'#root'} parent={this}
                     showTitle={true} 
                     title={this.t("msgCombineItem.title")} 
                     showCloseButton={false}
@@ -1484,8 +1483,7 @@ export default class itemEntryDoc extends React.PureComponent
                         <Column dataField="NAME" caption={this.t("pg_txtBarcode.clmName")} width={300} defaultSortOrder="asc" />
                     </NdPopGrid>
                     {/* Miktar Dialog  */}
-                    <NdDialog id={"msgQuantity"} container={'#' + this.props.data.id + this.tabIndex} parent={this}
-                    position={{at:'center center'}}
+                    <NdDialog id={"msgQuantity"} container={'#root'} parent={this}
                     showTitle={true} 
                     title={this.t("msgQuantity.title")} 
                     showCloseButton={false}
